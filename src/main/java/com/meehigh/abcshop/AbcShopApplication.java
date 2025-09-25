1a501d4852ee3122eeb8b065d5bc14bd68c15e84
zzarrykick <mihai.alexandru.calugaru@gmail.com>
2025-09-25 19:26:06 +0300

    Initial commit

---
+++ src/main/java/com/meehigh/abcshop/AbcShopApplication.java
@@ -0,0 +1,13 @@
+package com.meehigh.abcshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meehigh/abcshop/AbcShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meehigh.abcshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
